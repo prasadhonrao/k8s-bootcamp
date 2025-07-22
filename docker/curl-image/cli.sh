4c0696c879f6692ad78f00811bb80df61bf912c8
prasadhonrao <honrao.prasad@gmail.com>
2025-07-22 08:46:09 +0100

    Refactor Dockerfiles to use Alpine base image for smaller size and update CLI scripts for improved container management

--- docker/curl-image/cli.sh
+++ docker/curl-image/cli.sh
@@ -1,8 +1,10 @@
 docker build -t prasadhonrao/curl-image .
 
+docker image list | grep curl-image
+
 docker push prasadhonrao/curl-image
 
-docker run -d --name curl-container prasadhonrao/curl-image
+docker run -d --rm --name curl-container prasadhonrao/curl-image
 
 docker exec -it curl-container sh
 
@@ -11,6 +13,3 @@ curl -v microsoft.com
 exit
 
 docker stop curl-container
-
-docker rm curl-container
-
--- docker/nginx-custom-html/cli.sh
+++ docker/nginx-custom-html/cli.sh
@@ -1,14 +1,23 @@
 # Build the image
 docker build -t prasadhonrao/nginx-custom-html .
 
-# Check image status
-docker image history prasadhonrao/nginx-custom-html
+# Push the image to Docker Hub
+docker push prasadhonrao/nginx-custom-html
 
 # Create a container
-docker run --name nginx-custom-html --rm -p 80:80 prasadhonrao/nginx-custom-html
+docker run -d --rm --name nginx-custom-html -p 80:80 prasadhonrao/nginx-custom-html
+
+# Curl the container to check if it's running
+curl -v http://localhost
 
 # Exec into the container and check th HTML file
 docker exec -it nginx-custom-html sh
+
+# Check the HTML file
+cat /usr/share/nginx/html/index.html
+
+# Exit the container shell
+exit
 
 # Stop the container
 docker stop nginx-custom-html
